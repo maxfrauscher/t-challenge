489a7dad5b562cb60dd7c5dd118134c928b8f7ab
Maximilian <maxfrauscher.berlin@gmail.com>
2023-08-03 18:18:29 +0200

    feat(t-challenge): fetch user and user posts

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,7 +1,23 @@
-export default function User({ params }: any) {
+async function getUserById(id) {
+  'use server';
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  return res.json();
+}
+
+async function getPostsByUserId(id) {
+  'use server';
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+  return res.json();
+}
+
+export default async function User({ params }: any) {
+  const user = await getUserById(params.id);
+  const posts = await getPostsByUserId(params.id);
   return (
     <>
       <h1>User: {params.id}</h1>
+      <p>Name: {user.name}</p>
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
     </>
   );
 }
